0e76efb4cacd87667c628616613a8931427d72bc
Takuto Okuno <takuto.okuno.ku0823@gmail.com>
2021-03-06 09:21:27 +0900

    docs: add a problem statement

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -3,3 +3,9 @@ const sample = require("../app/sample.js")
 test('test 1.', () => {
     expect(sample.fizzBuzz(1)).toBe(1)
 })
+
+// FizzBuzz
+// 1から100までの数字を出力せよ。
+// ただし、3の倍数のときは "Fizz"、
+// 5の倍数のときは "Buzz"、
+// 3と5両方の倍数のときは "FizzBuzz" と出力せよ。
