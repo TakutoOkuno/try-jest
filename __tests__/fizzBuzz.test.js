b6e1805b5806dc89886c5cc25ef40cf6c9b53eaf
Takuto Okuno <takuto.okuno.ku0823@gmail.com>
2021-03-06 09:43:10 +0900

    test: add test that asserts convert received number to string

--- __tests__/fizzBuzz.test.js
+++ __tests__/fizzBuzz.test.js
@@ -14,6 +14,10 @@ test("test 1.", () => {
 // 3と5両方の倍数のときは数の代わりに "FizzBuzz"  に変換する。
 // テスト容易性：低 重要度：低
 // 1から100までの数字を出力せよ。
-test("これは失敗するはず", () => {
-  expect("string").toBe(1);
+test("数を文字列に変換する", () => {
+  // 準備
+  // 実行
+  const actual = sample.fizzBuzz(1)
+  // 検証
+  expect(actual).toBe("1");
 });
