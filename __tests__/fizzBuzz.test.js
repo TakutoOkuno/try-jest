0dba6d186ec84d99cecd6e553bc5b7e3f2c684ff
Takuto Okuno <takuto.okuno.ku0823@gmail.com>
2021-04-04 22:47:23 +0900

    feat: implement 'FizzBuzz' feature and its test

--- __tests__/fizzBuzz.test.js
+++ __tests__/fizzBuzz.test.js
@@ -9,8 +9,9 @@ const sample = require("../app/fizzBuzz.js");
 // 3の倍数のときは数の代わりに "Fizz" に変換する。
 // 3を渡すと文字列 "Fizz" を返す
 // 5の倍数のときは数の代わりに "Buzz" に変換する。
-  // 5を渡すと文字列 "Buzz" を返す
+// 5を渡すと文字列 "Buzz" を返す
 // 3と5両方の倍数のときは数の代わりに "FizzBuzz"  に変換する。
+// 15を渡すと文字列 "FizzBuzz" を返す
 // テスト容易性：低 重要度：低
 // 1から100までの数字を出力せよ。
 test('1を渡すと文字列 "1" に変換する', () => {
@@ -27,4 +28,9 @@ test('3を渡すと文字列 "Fizz" を返す', () => {
 
 test('5を渡すと文字列 "Buzz" を返す', () => {
   expect(sample.fizzBuzz(5)).toBe("Buzz");
+});
+
+test('15を渡すと文字列 "FizzBuzz" を返す', () => {
+  expect(sample.fizzBuzz(15)).toBe("FizzBuzz");
+  expect(sample.fizzBuzz(45)).toBe("FizzBuzz");
 });
